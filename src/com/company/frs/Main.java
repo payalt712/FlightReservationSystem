27ceef0c1129313fc74d2bac61dcae26e343c2b1
Payal <payalt712@gmail.com>
2021-03-03 16:10:39 +0530

    Some changes in main method as per question

--- src/com/company/frs/Main.java
+++ src/com/company/frs/Main.java
@@ -12,14 +12,21 @@ public class Main {
 
         Ticket regularTicket= new RegularTicket("ABC12035D","Delhi","Jabalpur","2021-02-15T14:30:00","2021-02-15T16:50:00","45E", false, 5000f, p,f1,"Cold Coffee with Peri Peri Fries" );
         Ticket touristTicket=new TouristTicket("ABC15523C","Dubai","Delhi", "2021-02-16T08:55:00", "2021:02:16T16:50:00", "33F", false, 250000f,p,f2, "Grand Hyatt,Near Sheikh Rashid Rd", new String[]{"1", "2", "3", "4"});
-        printTicketDetails(regularTicket, p, f1);
-        System.out.println("Journey Duration: "+ regularTicket.journeyDuration());
-        printTicketDetails(touristTicket,p, f2);
+
+        printTicketDetails(regularTicket);
+        printTicketDetails1(regularTicket, p, f1);
+
+        printTicketDetails(touristTicket);
+        printTicketDetails1(touristTicket,p, f2);
 
 
     }
 
-    private static void printTicketDetails(Ticket ticket, Passenger p, Flight f) {
-        System.out.println( p.getContact()+ "\nPNR: " +ticket.getPnr() + "\nFrom: "+ticket.getFrom()+ " To: "+ticket.getTo()+ "\nSeat Number: "+ticket.getSeatNumber()+ " Flight: "+f.getFlightNumber()+" "+f.getFlightName()+ "\nDeparture: "+ticket.getDepartureDetails()+" Arrival: "+ticket.getArrivalDetails() + "\n");
+    private static void printTicketDetails(Ticket ticket) {
+        System.out.println("PNR: "+ticket.getPnr());
+    }
+
+    private static void printTicketDetails1(Ticket ticket, Passenger p, Flight f) {
+        System.out.println( p.getContact()+ "\nFrom: "+ticket.getFrom()+ " To: "+ticket.getTo()+ "\nSeat Number: "+ticket.getSeatNumber()+ " Flight: "+f.getFlightNumber()+" "+f.getFlightName()+ "\nDeparture: "+ticket.getDepartureDetails()+" Arrival: "+ticket.getArrivalDetails() + "\n");
     }
 }
